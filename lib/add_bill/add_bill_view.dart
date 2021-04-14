4b668e469d55ae0815a0cb88d0bab0359e0a68c0
wenbo <jwenbo52@qq.com>
2021-04-14 12:12:58 +0800

    修改添加账单的样式

--- lib/add_bill/add_bill_view.dart
+++ lib/add_bill/add_bill_view.dart
@@ -9,6 +9,8 @@ class AddBillView extends StatefulWidget {
 }
 
 class _AddBillState extends State {
+  static const _BTN_PADDING = 36.0;
+  static const _BTN_RADIUS = 18.0;
   BillType _billType = BillType.expense;
   String _title = "支出";
 
@@ -24,6 +26,95 @@ class _AddBillState extends State {
   Widget _buildAddBillBody(BuildContext context) {
     final expenseColor = (_billType == BillType.expense ? Colors.green[300] : Colors.grey[400])!;
     final earningColor = (_billType == BillType.earning ? Colors.red[300] : Colors.grey[400])!;
+    return ListView(
+      children: [_billTypeView(expenseColor, earningColor), _billForm()],
+    );
+  }
+
+  Widget _billForm() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Form(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text("日期"),
+                ),
+                Expanded(
+                  child: TextFormField(
+                    initialValue: "当前时间",
+                    onTap: () {
+                      //弹出时间选择弹框
+                    },
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text("账户"),
+                ),
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(hintText: "请选择您的付款账户"),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text("分类"),
+                ),
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(hintText: "请选择账单类别"),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text("金额"),
+                ),
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(hintText: "输入付款金额"),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text("备注"),
+                ),
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(hintText: "添加账单备注"),
+                  ),
+                )
+              ],
+            ),
+            Center(
+              child: ElevatedButton(child: const Text("保存"), onPressed: () {}),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _billTypeView(Color expenseColor, Color earningColor) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -36,18 +127,18 @@ class _AddBillState extends State {
             ),
             onPressed: () => _onBillTypeChanged(BillType.expense),
             style: OutlinedButton.styleFrom(
-                padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                padding: const EdgeInsets.symmetric(horizontal: _BTN_PADDING),
                 side: BorderSide(color: expenseColor),
                 shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18.0), side: BorderSide(color: expenseColor)))),
+                    borderRadius: BorderRadius.circular(_BTN_RADIUS), side: BorderSide(color: expenseColor)))),
         OutlinedButton(
             child: Text("收入", style: TextStyle(color: earningColor)),
             onPressed: () => _onBillTypeChanged(BillType.earning),
             style: OutlinedButton.styleFrom(
-                padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                padding: const EdgeInsets.symmetric(horizontal: _BTN_PADDING),
                 side: BorderSide(color: earningColor),
                 shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18.0), side: BorderSide(color: earningColor)))),
+                    borderRadius: BorderRadius.circular(_BTN_RADIUS), side: BorderSide(color: earningColor)))),
         SizedBox(width: 45),
       ],
     );
